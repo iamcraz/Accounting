527b6a61ada9d053c780c60f36ba8ecdfd1e4763
Pooya <83201335+iamcraz@users.noreply.github.com>
2024-02-18 09:26:25 +0330

    Add files via upload

---
+++ Accounting_Viewmodel/Customers/ListCustomerViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting_Viewmodel.Customers
+{
+    public class ListCustomerViewModel
+    {
+        public string Customer_fullname
+        {
+            get;
+            set;
+        }
+
+        public int Customer_id
+        {
+            get;
+            set;
+        }
+    }
+}
